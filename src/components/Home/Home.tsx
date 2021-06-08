947c39f25265ea76c70fc96ad098bc94a8970f4f
Xavier Paolo Jamito <xavier@Xaviers-MacBook-Pro.local>
2021-06-08 12:44:52 +0800

    add validation for username input

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,45 @@
-import React from "react";
-import { HomeContainer } from './Home.styled';
-import { Input } from 'antd';
+import React, { useState } from "react";
+import { HomeContainer } from "./Home.styled";
+import { Input, Typography } from "antd";
 
 const Home = () => {
+  const [username, setUsername] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const { Search } = Input;
+  const { Text } = Typography;
+
+  const onSearch = (searchQuery: string) => {
+    console.log(searchQuery);
+  }
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const username = e.currentTarget.value;
+    if (username && !!username.match(/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i)) {
+      setUsername(username);
+      setErrorMessage("");
+    } else {
+      if (username) {
+        setErrorMessage("Special characters and spaces are not allowed.");
+        username.length >= 39 && setErrorMessage("Username can only have 39 characters.")
+      } else {
+        setUsername("");
+      }
+    }
+  }
+
   return (
     <HomeContainer>
-      <Search placeholder="Search for Github username here" allowClear enterButton="Search" size="large"/>
+      <h2> Github Repo Search </h2>
+      { errorMessage && <Text type="danger">{ errorMessage }</Text> }
+      <Search
+        placeholder="Search for Github username here"
+        allowClear
+        enterButton="Search"
+        size="large"
+        onSearch={onSearch}
+        onChange={onChangeInput}
+        value={username}
+      />
     </HomeContainer>
   )
 }
