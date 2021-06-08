c0141692f8b559ee046ba81b54508f3db5c8f28a
Xavier Paolo Jamito <xavierjamito@gmail.com>
2021-06-08 22:40:04 +0800

    add table to view user repos data

--- src/components/Repos/Repos.tsx
+++ src/components/Repos/Repos.tsx
@@ -1,22 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { getUserRepos } from "../api/UserAPI";
-import { Parameter } from "../../models/types";
+import { Parameter, Repo } from "../../models/types";
+import { Table, Spin } from 'antd';
 
 const Repos = () => {
   const { username } = useParams<Parameter>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [repos, setRepos] = useState<Repo[]>([]);
 
   useEffect(() => {
     const fetchUserRepos = async () => {
+      setIsLoading(true);
       const userRepos = await getUserRepos(username);
+      setRepos(userRepos);
       console.log(userRepos);
+      setIsLoading(false);
     }
     fetchUserRepos();
   }, [username]);
 
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      // render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Starred Count',
+      dataIndex: 'starred',
+      key: 'starred',
+    },
+    {
+      title: 'Link',
+      dataIndex: 'link',
+      key: 'link',
+    },
+  ];
+
+  const repoData = repos.map((repoItem: Repo, index: number) => ({
+    key: index,
+    name: repoItem.name,
+    description: repoItem.description,
+    starred: repoItem.stargazers_count,
+    link: repoItem.html_url,
+  }))
+
   return (
     <div>
       <h1>Repos</h1>
+      {
+        isLoading ? (
+          <Spin />
+        ) : (
+          <Table key="key" columns={columns} dataSource={repoData} />
+        )
+      }
     </div>
   )
 }
--- src/models/types.ts
+++ src/models/types.ts
@@ -1,3 +1,10 @@
 export type Parameter = {
   username: string;
 }
+
+export interface Repo {
+  name: string;
+  description: string;
+  stargazers_count: number;
+  html_url: string;
+}
